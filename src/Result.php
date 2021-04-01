14add9c8058d2be215e805e69296abb1d1a233b0
vitaliy bondarenko <vitaliy.b@zone3000.net>
2021-04-01 21:40:11 +0300

    Class created

---
+++ src/Result.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Vbcod;
+
+class Result
+{
+    const STATUS_UNDEFINED = 'undefined';
+    const STATUS_SUCCESS = 'success';
+    const STATUS_ERROR = 'error';
+
+    public $status = self::STATUS_UNDEFINED;
+    public $message = '';
+
+    public function isSuccess()
+    {
+        return $this->status == self::STATUS_SUCCESS;
+    }
+
+    public function setStatusToSuccess()
+    {
+        $this->status = self::STATUS_SUCCESS;
+        return $this;
+    }
+
+    public function setStatusToError()
+    {
+        $this->status = self::STATUS_ERROR;
+        return $this;
+    }
+
+    public function setMessage($message = '')
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
